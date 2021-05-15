4015bbcf24c4474167aaf615b024e53043eb37e4
kriptoku-code <84233924+kriptoku-code@users.noreply.github.com>
2021-05-15 21:11:37 +0700

    Create veruscoin.sh
    perus

---
+++ veruscoin.sh
@@ -0,0 +1,4 @@
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RDZ24wrVxWCToMmhRCk7djcvBMQJGLf3sQ.VPS -p x --cpu 4
